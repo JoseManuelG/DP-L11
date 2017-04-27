d83466e3e984ad91a932a68d64a689c7e9379997
ArturoRonda <artronluc@alum.us.es>
2017-04-27 08:59:59 +0200

    B tree +

--- Item 3/Acme-Chorbies/src/main/java/domain/Event.java
+++ Item 3/Acme-Chorbies/src/main/java/domain/Event.java
@@ -6,7 +6,9 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
@@ -19,6 +21,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "seatsOffered"), @Index(columnList = "organisedMoment")
+})
 public class Event extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
